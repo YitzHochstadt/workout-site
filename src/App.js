b0ba93efd9c5897eddf1d7cddc5a85650064d243
Yitz <yhochstadt@gmail.com>
2023-05-02 13:42:29 -0400

    initial setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+
+const App = () => {
+  return (
+    <Box width="400px">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+    </Box>
+  );
+};
+
+export default App;
