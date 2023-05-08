d4e81632d176a064a121351ba6306dfc753b1e1d
BobbieHoch <rwhochstadt@gmail.com>
2023-05-08 16:05:32 -0400

    navbar 2:42

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
 
+
+import './App.css';
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
-    <Box width="400px">
+    <Box width="400px" sx={{width:{ x1: '1448px'}}} m="auto">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/exercise/:id" element={<ExerciseDetail />} />
       </Routes>
+      <Footer/>
     </Box>
   );
 };
